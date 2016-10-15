4681ae7b6ce25651f980fb7470676909cc5e3c24
Morgan Touverey-Quilling <mtouverey@methodinthemadness.eu>
2016-10-15 14:29:22 +0200

    Handle multiple databases

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 
-const DB_YAML = __DIR__.'/database.yml';
-const DB_JSON = __DIR__.'/database.compiled.json';
-
 $app = new Silex\Application;
 
 $app['debug'] = getenv('SILEX_PROD') == false;
 
-$app->get('/database.json', function() {
-    if(!file_exists(DB_JSON)) {
-        $database = Yaml::parse(file_get_contents(DB_YAML));
-        file_put_contents(DB_JSON, $json = json_encode($database, JSON_PRETTY_PRINT));
+$app->get('/{dbname}.json', function($dbname) {
+    $yamldb = __DIR__."/{$dbname}.yaml";
+    $jsondb = __DIR__."/{$dbname}.compiled.json";
+
+    if(!file_exists($jsondb)) {
+        $database = Yaml::parse(file_get_contents($yamldb));
+        file_put_contents($jsondb, $json = json_encode($database, JSON_PRETTY_PRINT));
     }
 
-    return $json ?? file_get_contents(DB_JSON);
+    return $json ?? file_get_contents($jsondb);
 });
 
 $app->run();
